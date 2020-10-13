acf42e411392d09af933a9aaf6c14a63520dd266
Kristoffer Andreasen <kan@clever.dk>
2020-10-13 16:26:35 +0200

    renamed folder

--- src/ElectricityMap.DotNet.Client/Interfaces/IElectricityMapClient.cs
+++ src/ElectricityMap.DotNet.Client/Interfaces/IElectricityMapClient.cs
@@ -1,4 +1,10 @@
 ﻿using ElectricityMap.DotNet.Client.Models;
+using ElectricityMap.DotNet.Client.Models.Forecasts;
+using ElectricityMap.DotNet.Client.Models.History;
+using ElectricityMap.DotNet.Client.Models.Live;
+using ElectricityMap.DotNet.Client.Models.Recent;
+using ElectricityMap.DotNet.Client.Models.Updates;
+using ElectricityMap.DotNet.Client.Models.Zones;
 using System;
 using System.Threading.Tasks;
 
@@ -7,7 +13,7 @@ namespace ElectricityMap.DotNet.Client.Interfaces
     public interface IElectricityMapClient
     {
         Task<Zones> GetZonesAsync();
-        Task<LiveCarbonIntentsity> GetLiveCarbonIntensityAsync(string zone, double? longitude, double? latitude);
+        Task<LiveCarbonIntensity> GetLiveCarbonIntensityAsync(string zone, double? longitude, double? latitude);
         Task<LivePowerBreakdown> GetLivePowerBreakdownAsync(string zone, double? longitude, double? latitude);
         Task<RecentCarbonIntensityHistory> GetRecentCarbonIntensityHistoryAsync(string zone, double? longitude, double? latitude);
         Task<RecentPowerBreakdownHistory> GetRecentPowerBreakdownHistoryAsync(string zone, double? longitude, double? latitude);
