0db04f3af4e61f681d9036e8d2d0997fe6d23186
Kristoffer Andreasen <kan@clever.dk>
2021-04-15 17:32:08 +0200

    facade tests

---
+++ test/ElectricityMap.DotNet.Client.Test/Http/ElectricityMapHttpFacadeTests.cs
@@ -0,0 +1,25 @@
+﻿using AutoFixture.Xunit2;
+using ElectricityMap.DotNet.Client.Http;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace ElectricityMap.DotNet.Client.Test.Http
+{
+    public class ElectricityMapHttpFacadeTests
+    {
+        [Fact]
+        public void No_key_throws_exception()
+            => Assert
+            .Throws<ArgumentNullException>(() => new ElectricityMapHttpFacade(null));
+
+        [Theory, AutoData]
+        public void Http_facade_is_created(
+            string apiKey)
+        {
+            var sut = new ElectricityMapHttpFacade(apiKey);
+
+            sut.Should().NotBeNull();
+        }
+    }
+}
