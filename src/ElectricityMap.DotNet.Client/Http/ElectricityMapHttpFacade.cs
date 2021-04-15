61ccd4e4002df3c7823265f5cf04deaa0d388412
Kristoffer Andreasen <kan@clever.dk>
2021-04-15 20:45:20 +0200

    refactor facade

--- src/ElectricityMap.DotNet.Client/Http/ElectricityMapHttpFacade.cs
+++ src/ElectricityMap.DotNet.Client/Http/ElectricityMapHttpFacade.cs
@@ -1,13 +1,15 @@
 ﻿using ElectricityMap.DotNet.Client.Constants;
+using ElectricityMap.DotNet.Client.Exceptions;
+using Newtonsoft.Json;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace ElectricityMap.DotNet.Client.Http
 {
-    public class ElectricityMapHttpFacade
+    public class ElectricityMapHttpFacade : IElectricityMapHttpFacade
     {
-        private readonly ElectricityMapApiRequestor requestor;
+        private readonly HttpClient httpClient;
 
         public ElectricityMapHttpFacade(string apiKey)
         {
@@ -16,16 +18,49 @@ namespace ElectricityMap.DotNet.Client.Http
                 throw new ArgumentNullException(apiKey, "You must pass a valid API key to access the Electricity Map.");
             }
 
-            HttpClient httpClient = new HttpClient
+            httpClient = new HttpClient
             {
                 BaseAddress = new Uri(ApiConstants.BaseUrl)
             };
             httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
             httpClient.DefaultRequestHeaders.Add(ApiConstants.AuthHeader, apiKey);
-            requestor = new ElectricityMapApiRequestor(httpClient);
         }
 
-        public Task<T> GetAsync<T>(string url)
-            => requestor.GetAsync<T>(url);
+        public async Task<T> GetAsync<T>(string url)
+        {
+            HttpResponseMessage responseMessage = await httpClient
+                .GetAsync(url).ConfigureAwait(false);
+
+            await EnsureSuccessStatusCodeAsync(responseMessage)
+                .ConfigureAwait(false);
+
+            return await ReadResponseAsync<T>(responseMessage)
+                .ConfigureAwait(false);
+        }
+
+        private static async Task<T> ReadResponseAsync<T>(HttpResponseMessage responseMessage)
+        {
+            string response = await responseMessage.Content
+                .ReadAsStringAsync()
+                .ConfigureAwait(false);
+
+            var result = JsonConvert.DeserializeObject<T>(response);
+
+            return result;
+        }
+
+        private static async Task EnsureSuccessStatusCodeAsync(HttpResponseMessage responseMessage)
+        {
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return;
+            }
+
+            string errorResponse = await responseMessage.Content
+                .ReadAsStringAsync()
+                .ConfigureAwait(false);
+
+            throw new ElectricityMapException(responseMessage.StatusCode, errorResponse ?? string.Empty);
+        }
     }
 }
---
+++ src/ElectricityMap.DotNet.Client/Http/IElectricityMapHttpFacade.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ElectricityMap.DotNet.Client.Http
+{
+    public interface IElectricityMapHttpFacade
+    {
+        Task<T> GetAsync<T>(string url);
+    }
+}
